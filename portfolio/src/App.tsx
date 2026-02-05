1cf7f7100c612470ffca00ca53bd20c2473fa97f
Jonathan H <138881804+hognelid@users.noreply.github.com>
2026-02-05 16:30:08 +0100

    Card update

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/card/card'
+import * as Core from "@sebgroup/green-core/react"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
   <Card/>
+  <Core.GdsButton>hello</Core.GdsButton>
     </>
   )
 }
